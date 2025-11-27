70ec22c034c0903edb93af9ca6514074bc3d0a7c
JACV04 <ja.calleros24@info.uas.edu.mx>
2025-11-26 17:55:42 -0700

    se agrego codigo a los script cart

--- proyecto de verdad/JS/cart.js
+++ proyecto de verdad/JS/cart.js
@@ -5,6 +5,8 @@ function crearTarjetaProductosInicio(productos){
     productos.forEach(producto => {
         const nuevoProducto = document.createElement("div");
         nuevoProducto.classlist = "tarjeta-producto";
+        nuevoProducto.innerHTML = `
+            <img src= "/EJEMPLO-DE-PROYECTO-JEJE/images/h610 intel lga 1700 mini tx.jpg${producto.id}.jpg">
         contenedorProductos.appendChild(nuevoProducto);
     });  
 }
@@ -12,4 +14,3 @@ function crearTarjetaProductosInicio(productos){
 
 
 crearTarjetaProductosInicio(productos);
-
