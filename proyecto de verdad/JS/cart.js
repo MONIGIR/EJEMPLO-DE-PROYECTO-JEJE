460f18b49328f2fd9f3f15ae6b3e7a33eb9005f3
JACV04 <ja.calleros24@info.uas.edu.mx>
2025-11-27 10:45:08 -0700

    se modifico los scritp cart y  productos como styles y html del carrito

--- proyecto de verdad/JS/cart.js
+++ proyecto de verdad/JS/cart.js
@@ -4,9 +4,13 @@ const contenedorProductos = document.getElementById("productos-conteiner");
 function crearTarjetaProductosInicio(productos){
     productos.forEach(producto => {
         const nuevoProducto = document.createElement("div");
-        nuevoProducto.classlist = "tarjeta-producto";
+        nuevoProducto.className = "tarjeta-producto";
         nuevoProducto.innerHTML = `
-            <img src= "/EJEMPLO-DE-PROYECTO-JEJE/images/h610 intel lga 1700 mini tx.jpg${producto.id}.jpg">
+            <img src= "/EJEMPLO-DE-PROYECTO-JEJE/images/h610 intel lga 1700 mini tx.jpg${producto.id}" alt="${producto.nombre}">
+            <h3>${producto.nombre}</h3>
+            <p>Precio: $${producto.precio}</p>
+            <button class="agregar-carrito" data-id="${producto.id}">Agregar al carrito</button>
+        `;
         contenedorProductos.appendChild(nuevoProducto);
     });  
 }
--- proyecto de verdad/JS/productos.js
+++ proyecto de verdad/JS/productos.js
@@ -3,6 +3,6 @@ const productos= [
         id: 1,
         nombre: "Gigabyte H610 Intel LGA 1700 MINI-TX",
         precio: 3500,
-        imagen: "/EJEMPLO-DE-PROYECTO-JEJE/images/h610 intel lga 1700 mini tx.jpg"
+        imagen: "proyecto de verdad/images/gigabyte h610 intel lga 1700 mini tx.jpg intel lga 1700 mini tx.jpg"
     }
 ]
