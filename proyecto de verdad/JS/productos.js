e6f6a4e4f356fb573ae1ba437542733ff0b82085
JACV04 <ja.calleros24@info.uas.edu.mx>
2025-11-27 22:30:59 -0700

    se modifico el html de productos y cart al igual que se agrego un nuevo stilo para productos

--- proyecto de verdad/JS/productos.js
+++ proyecto de verdad/JS/productos.js
@@ -3,6 +3,12 @@ const productos= [
         id: 1,
         nombre: "Gigabyte H610 Intel LGA 1700 MINI-TX",
         precio: 3500,
-        imagen: "proyecto de verdad/images/gigabyte h610 intel lga 1700 mini tx.jpg intel lga 1700 mini tx.jpg"
+        imagen: "/proyecto de verdad/images/gigabyte h610 intel lga 1700 mini tx.jpg"
+    },
+    {
+        id: 2,
+        nombre: "2",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/7 5700X3D-900x900.jpg"
     }
 ]
