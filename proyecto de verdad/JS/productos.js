bf36443a951f859faee943602f31d1d71af17bfd
JACV04 <ja.calleros24@info.uas.edu.mx>
2025-12-04 14:01:31 -0700

    se agregaron tos los productgos al script productos

--- proyecto de verdad/JS/productos.js
+++ proyecto de verdad/JS/productos.js
@@ -10,10 +10,172 @@ const productos= [
     },
     {
         id: 2,
-        nombre: "hola mundo",
+        nombre: "RYZEN 7 5700X3D",
         precio: 55555,
         imagen: "/proyecto de verdad/images/7 5700X3D-900x900.jpg"
+    },
+
+    {
+        id: 3,
+        nombre: "32GB Viper Steel",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/32 gb Viper Steel.jpg"
+    },
+    
+    {
+        id: 4,
+        nombre: "GIGABYTR GeForce RTX 3060 OC 12GB",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/3060 12 gb.jpg"
+    },
+
+    {
+        id: 5,
+        nombre: "GIGABYTR GeForce RTX 3080 OC 12GB",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/3080 12 g.jpg"
+    },
+
+    {
+        id: 6,
+        nombre: "MSI GeForce RTX 4060 VENTUS 2X Black OC 8G",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/4060 8g.jpg"
+    },
+
+    {
+        id: 7,
+        nombre: "ASUS TUF GAMING GeForce 5070 OC 12GB",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/5070 12 gb.jpg"
+    },
+
+    {
+        id: 8,
+        nombre: "GIGABYTE GeForce RTX 5070 Ti OC 16GB",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/5070 ti 16 gb.jpg"
+    },
+
+    {
+        id: 9,
+        nombre: "MSI GeForce RTX 5090 GAMING TRIO OC 32GB",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/5090 32 g.jpg"
+    },
+
+    {
+        id: 10,
+        nombre: "MSI AMD B550 GAMING PLUS",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/amd b550 gaming plus.jpg"
+    },
+
+    {
+        id: 11,
+        nombre: "AORUS Z690 MASTER LGA 1700 INTEL",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/aours z690.jpg"
+    },
+
+    {
+        id: 12,
+        nombre: "GIGABYTE B550 AM4",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/b550 am4.jpg"
+    },
+
+    {
+        id: 13,
+        nombre: "CORSAIR VENGEACE RGB PRO DDR4 32GB",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/corsair vengeance rgb pro ddr4 32 gb.jpg"
+    },
+
+    {
+        id: 14,
+        nombre: "GIBABYTE B850 EAGLE AM5",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/Gigabyte B850 Eagle AM5.webp"
+    },
+
+    {
+        id: 15,
+        nombre: "GIGABYTE H610 INTEL LGA 1700 MINI TX",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/gigabyte h610 intel lga 1700 mini tx.jpg"
+    },
+
+    {
+        id: 16,
+        nombre: "GIGABYTE Z790 EAGLE LGA 1700 AX",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/gigabyte z790 eagle ax lga 1700.webp"
+    },
+
+    {
+        id: 17,
+        nombre: "INTEL i7 14TH",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/intel i 7 14th.jpg"
+    },
+
+    {
+        id: 18,
+        nombre: "INTEL i9 14TH",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/intel i9 14th.jpg"
+    },
+
+    {
+        id: 19,
+        nombre: "PATRIOT STEEL DDDR4 16GB",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/patriot steel ddr4 ram 16g.jpg"
+    },
+
+    {
+        id: 20,
+        nombre: "KINGTON FURY DDRA4 32GB",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/ram fury 32gb ddr4 3000mhz.png"
+    },
+
+    {
+        id: 21,
+        nombre: "VIPER STEEL ELITE DDR4 2X16GB",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/Ram viper steel elite 2 ddr4 2666mhz 16 gb.jpg"
+    },
+
+    {
+        id: 22,
+        nombre: "RYZEN 7 5700X",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/ryzen 7 5700x.jpg"
+    },
+
+    {
+        id: 23,
+        nombre: "RYZEN 7 7800X3D",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/ryzen 7 7800x3d.jpg"
+    },
+
+    {
+        id: 24,
+        nombre: "XPG 16GB DDR5",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/XPG 16GB DDR5.jpg"
+    },
+
+    {
+        id: 25,
+        nombre: "KINGTON M.2 2TB",
+        precio: 55555,
+        imagen: "/proyecto de verdad/images/KINGSTON M.2 2TB.jpg"
     }
 
+
+
 ]
-s
